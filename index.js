aee8732e56a78d221e23b376c3a4f013de82062f
yemulyuvraj <163127578+yemulyuvraj@users.noreply.github.com>
2025-03-12 11:12:08 +0530

    vinod

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var dotenv = require('dotenv');
 var upload = require("express-fileupload");
 var session = require("express-session");
-const path = require('path');
-dotenv.config();
+var user_route = require("./routes/user_route");
+var admin_route = require("./routes/admin_route");
 var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(upload());
@@ -14,18 +13,7 @@ app.use(session({
     saveUninitialized: true
 }));
 app.use(express.static("public/"));
-app.use("/uploads", express.static(path.join(__dirname, 'uploads')));
-var user_route = require("./routes/user_route");
-var admin_route = require("./routes/admin_route");
+
 app.use("/", user_route);
 app.use("/admin", admin_route);
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
-});
-
-// Start the server
-const PORT = process.env.PORT || 2000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+app.listen(1000)
--- routes/admin_route.js
+++ routes/admin_route.js
@@ -1,5 +1,6 @@
 var express=require("express");
 var exe=require("./../conn");
+const { log } = require("util");
 var router=express.Router();
 router.get("/admin_login",function(req,res){
     res.render("admin/admin_login.ejs");
@@ -12,24 +13,24 @@ router.get("/",function(req,res){
     res.render("admin/home.ejs");
     }
 });
-router.get('/logout',  function (req, res, next)  {
-    if (req.session) {
-      req.session.destroy(function (err) {
-        if (err) {
-          return next(err);
-        } else {
-          return res.redirect('/admin/admin_login');
-        }
-      });
+function checkLogin(req, res, next) {
+    // req.session.admin_login_id=1;
+    if (req.session.user_id == undefined) {
+      res.redirect("/admin/admin_login");
+    } else {
+      next();
     }
+  }
+router.get("/logout_admin", function (req, res) {
+    req.session.destroy(function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect("/admin/admin_login");
+        }
+    });
 });
-function checklogin(req, res, next) 
-{
-    if(req.session.user_id != undefined)
-        next();
-    else 
-        res.send("<script> alert('Invalid Login');location.href='/admin/admin_login';</script>")        
-}
+
 router.post("/admin_process",async function(req,res){
     var d=req.body;
     var sql=`SELECT * FROM admin_login WHERE username = '${d.username}' AND  password = '${d.password}'`;
@@ -44,60 +45,230 @@ router.post("/admin_process",async function(req,res){
      
      }   
 })
-router.get("/manage_home_page",checklogin,async function(req,res){
+router.get("/manage_home_page",checkLogin,async function(req,res){
     var sql=`SELECT * FROM home_page `;
     var data=await exe(sql);
     var obj={"home":data[0]}
     res.render("admin/manage_home_page.ejs",obj)
 })
-router.post("/update_home_page",async function(req,res){
+// home page update
+router.post("/update_home_page",checkLogin, async function (req, res) {
+    var d = req.body;
+    var file_name = "";
+    function sanitizeInput(input) {
+        return input ? input.replaceAll("'", "").replaceAll('"', "").trim() : null;
+    }
+    if (req.files && req.files.backgound_image) {
+        file_name = new Date().getTime() + ".png";
+        req.files.backgound_image.mv("public/uploads/" + file_name);
+        var sql2 = `UPDATE home_page SET backgound_image = '${file_name}' WHERE user_id = 1`;
+        await exe(sql2);
+    }
+    var sql = `UPDATE home_page SET 
+        user_name_in_short = ${d.user_name_in_short ? `'${sanitizeInput(d.user_name_in_short)}'` : "NULL"}, 
+        user_name = ${d.user_name ? `'${sanitizeInput(d.user_name)}'` : "NULL"},  
+        position_1 = ${d.position_1 ? `'${sanitizeInput(d.position_1)}'` : "NULL"}, 
+        position_2 = ${d.position_2 ? `'${sanitizeInput(d.position_2)}'` : "NULL"}, 
+        position_3 = ${d.position_3 ? `'${sanitizeInput(d.position_3)}'` : "NULL"} 
+    WHERE user_id = 1`;
+    await exe(sql);
+    res.redirect("/admin/manage_home_page");
+});
+// about section
+router.get("/manage_about_page",checkLogin, async function(req, res) {
+    var sql = `SELECT * FROM about_section`;
+    var skill = `SELECT * FROM skills`;
+
+    var data = await exe(sql);
+    var skillsData = await exe(skill); 
+
+    var obj = { "about": data[0], "skills": skillsData };
+    res.render("admin/manage_about_page.ejs", obj);
+});
+
+router.post("/update_about", checkLogin,async (req, res) => {
+    let d = req.body;
+    let about_profile_name = d.about_profile_name.replaceAll("'", "''");
+    let about_profile_position = d.about_profile_position.replaceAll("'", "''");
+    let about_profile_email = d.about_profile_email.replaceAll("'", "''");
+    let about_profile_mo = d.about_profile_mo.replaceAll("'", "''");
+    let about_profile_summary = d.about_profile_summary.replaceAll("'", "''");
+    let profile_photo = "";
+    if (req.files && req.files.about_profile_photo) {
+        let file = req.files.about_profile_photo;
+        profile_photo = new Date().getTime() + "_" + file.name;
+        file.mv("public/uploads/" + profile_photo);
+    }
+    let sql;
+    if (profile_photo !== "") {
+        sql = `UPDATE about_section SET 
+            about_profile_name = '${about_profile_name}',
+            about_profile_position = '${about_profile_position}',
+            about_profile_email = '${about_profile_email}',
+            about_profile_mo = '${about_profile_mo}',
+            about_profile_summary = '${about_profile_summary}',
+            about_profile_photo = '${profile_photo}'
+            WHERE id = 1`;
+    } else {
+        sql = `UPDATE about_section SET 
+            about_profile_name = '${about_profile_name}',
+            about_profile_position = '${about_profile_position}',
+            about_profile_email = '${about_profile_email}',
+            about_profile_mo = '${about_profile_mo}',
+            about_profile_summary = '${about_profile_summary}'
+            WHERE id = 1`;
+    }
+    await exe(sql);
+    res.redirect("/admin/manage_about_page");
+});
+// skill section
+router.post("/add_skill",checkLogin, async(req, res) => {
+    let d = req.body;
+    let tech_name = d.tech_name.replaceAll("'", "''");
+    let tech_range = d.tech_range.replaceAll("'", "''");
+    let sql = `INSERT INTO skills (tech_name, tech_range) VALUES ('${tech_name}', '${tech_range}')`;
+    var data=await exe(sql);
+    res.redirect("/admin/manage_about_page");
+});
+// delete skill
+router.get("/delete_skill/:id",checkLogin,async function(req,res){
+var data=await exe(`DELETE FROM skills WHERE id='${req.params.id}'`);
+res.redirect("/admin/manage_about_page")
+});
+// edit skill
+router.get("/edit_skill/:id",checkLogin,async function(req,res){
+    var data=await exe(`SELECT * FROM skills WHERE id ='${req.params.id}'`);
+    res.render("admin/edit_skill.ejs",{"skills":data[0]});
+});
+router.post("/update_skill/:id",checkLogin,async function(req,res){
     var d=req.body;
-    if(req.files)
-    {
+    var sql=`UPDATE skills SET tech_name = ? , tech_range = ? WHERE id ='${req.params.id}'`;
+    var data=await exe(sql,[d.tech_name,d.tech_range]);
+    res.redirect("/admin/manage_about_page");
+});
+// edit skill end
+
+// project section start
+router.get("/project_section",checkLogin,async function(req,res){
+    var data=await exe(`SELECT * FROM project_section`);
+    res.render("admin/project_section.ejs",{projects:data});
+});
+router.post("/save_project",checkLogin, async function (req, res) {
+    var d = req.body;
+    var sql = `INSERT INTO project_section (project_link, project_name, project_description) VALUES (?, ?, ?)`;
+    var data = await exe(sql, [d.project_link, d.project_name, d.project_description]);
+    res.redirect("/admin/project_section");
+});
+router.get("/delete_project/:id",checkLogin, async function(req,res){
+    var data=await exe(`DELETE FROM project_section WHERE id ='${req.params.id}'`);
+    res.redirect("/admin/project_section");
+})
+// project section end
+
+// couner sections start
+router.post("/admin_update_count_of_down_of_project_secton", checkLogin,async (req, res) => {
+    const {  project_completed, y_of_exp, total_clients } = req.body;
+    const sql = "UPDATE project_counts SET project_completed = ?, years_of_experience = ?, total_clients = ? WHERE id = 1";
+    await exe(sql, [project_completed, y_of_exp, total_clients]);
+    res.redirect("/admin/project_section");
+});
+// couner sections end
+
+// internship start
+router.get("/internship",checkLogin,async function(req,res){
+    var data=await exe(`SELECT * FROM intern_and_web_dev`);
+    res.render("admin/internship.ejs",{"intern_and_web":data});
+});
+
+// Route to update certificates
+router.post("/internship_and_web_dev_certificate",checkLogin, async function (req, res) {
+    if (!req.files) {
+        res.send("No files uploaded");
+    } else {
+        var internship_certificate = "";
+        var web_developer_certificate = "";
+
+        // Internship Certificate
+        if (req.files.internship_certificate) {
+            internship_certificate = new Date().getTime()+".png";
+            req.files.internship_certificate.mv("public/uploads/" + internship_certificate);
+        }
+
+        // Web Developer Certificate
+        if (req.files.web_developer_certificate) {
+            web_developer_certificate =new Date().getTime()+".png";
+            req.files.web_developer_certificate.mv("public/uploads/" + web_developer_certificate);
+        }
+
+        // Update record (Assuming only one record with ID = 1)
+        var updateSQL = `UPDATE intern_and_web_dev SET 
+            internship_certificate = ?, 
+            web_developer_certificate = ? 
+            WHERE id = 1`;  // Change ID based on your logic
+
+        await exe(updateSQL, [internship_certificate, web_developer_certificate]);
+
+        res.redirect("/admin/internship");
+    }
+});
+
+// certificate start
+router.post("/add_certificate",checkLogin,function(req,res){
+    // console.log(req.files)
+    var file_name="";
+    if(req.files){
+        file_name=new Date().getTime()+".png";
+        req.files.certificate.mv("public/uploads/"+file_name);
+        var sql=`INSERT INTO a2z_certificate(id INT PRIMARY KEY AUTO_INCREMENT, certificate TEXT ,tech_name TEXT )`
+    }
+});
+router.get("/certificate_section",checkLogin,async function(req,res){
+    var data=await exe(`SELECT * FROM a2z_certificate`)
+    res.render("admin/certificate_section.ejs",{"certificate":data});
+});
+router.post("/save_all_certificates",checkLogin,async function(req,res){
+    var d=req.body;
     var file_name=new Date().getTime()+".png";
-    req.files.backgound_image.mv("public/uploads/"+file_name);
-    var sql2=`UPDATE home_page SET backgound_image = '${file_name}' WHERE user_id = 1`;
-    var data = await exe(sql2);
-    }
-    else
-    {
-    var sql=`UPDATE home_page SET user_name_in_short = '${d.user_name_in_short}' , user_name = '${d.user_name}'  , position_1 = '${d.position_1}' , position_2 = '${d.position_2}' , position_3 = '${d.position_3}'WHERE user_id = 1 `;
+    req.files.certificate.mv("public/uploads/"+file_name);
+    var sql=`INSERT INTO a2z_certificate(certificate, tech_name) VALUES(?,?)`;
+    var data=await exe(sql,[file_name,d.tech_name])
+  res.redirect("/admin/certificate_section");
+   
+});
+router.get("/delete_a2z_certificate/:id",checkLogin,async function(req,res){
+    var data=await exe(`DELETE FROM a2z_certificate WHERE id ='${req.params.id}' `);
+    res.redirect("/admin/certificate_section");
+})
+// certificate end
 
-    var data=await exe(sql);
-    }
-    res.redirect("/admin/manage_home_page")
-});
-router.get("/manage_about_page",checklogin,async function(req,res){
-    var sql=`SELECT * FROM about_page`;
-    var data=await exe(sql);
-    var obj={"about":data[0]};
-    res.render("admin/manage_about_page.ejs",obj)
-});
-router.post("/update_about_page",async function(req,res){
-    var d=req.body;
-    if(req.files)
-        {
-        var file_name=new Date().getTime()+".png";
-        req.files.profile_photo.mv("public/uploads/"+file_name);
-        var sql2=`UPDATE about_page SET profile_photo = '${file_name}' WHERE user_id = 1`;
-        var data = await exe(sql2);
-        }
-        else
-        {
-        var sql=`UPDATE about_page SET  profile_name = '${d.profile_name}' , profile_position = '${d.profile_position}' , profile_email = '${d.profile_email}' , profile_phone = '${d.profile_phone}' , skill_persentage_html = '${d.skill_persentage_html}', skill_persentage_css = '${d.skill_persentage_css}' ,skill_persentage_bootstrap = '${d.skill_persentage_bootstrap}' , skill_persentage_javascript = '${d.skill_persentage_javascript}', skill_persentage_jquery = '${d.skill_persentage_jquery}', skill_persentage_angular = '${d.skill_persentage_angular}', skill_persentage_ionic = '${d.skill_persentage_ionic}' ,skill_persentage_node_express = '${d.skill_persentage_node_express}' , skill_persentage_mysql = '${d.skill_persentage_mysql}' , profile_user_desc  = '${d.profile_user_desc}'  WHERE user_id = 1 `;
 
-        }
-    var data=await exe(sql);
-    // res.send(data);
-    res.redirect("/admin/manage_about_page")
-});
-router.get("/manage_certificate_page",checklogin,async function(req,res){
+
+
+
+
+
+
+// router.post("/upload_certificate",async function(req,res){
+//     var d=req.body;
+//     var file_name="";
+//     if(req.files){
+//         file_name=new Date().getTime()+"png";
+//         req.files.client_photo.mv("public/uploads/"+file_name);
+//         var sql=`INSERT INTO client_information(client_photo  , client_name  , client_inform ) VALUES (?,?,?)`;
+//         var data=await exe(sql,[file_name,d.client_name,d.client_inform]);
+//     }
+//     else{
+//         var sql=`INSERT INTO client_information(client_photo  , client_name  , client_inform ) VALUES (?,?,?)`;
+//         var data=await exe(sql,[file_name,d.client_name,d.client_inform]);
+//     }
+// })
+router.get("/manage_certificate_page",checkLogin,async function(req,res){
     var sql=`SELECT * FROM certificate_page`;
     var data=await exe(sql);
     var obj={"certificate":data[0]}
     res.render("admin/manage_certificate_page.ejs",obj)
 });
-router.post("/update_certificate_page",checklogin, async function(req,res){
+router.post("/update_certificate_page",checkLogin, async function(req,res){
     var d=req.body;
     if(req.files)
     {
@@ -180,75 +351,75 @@ router.post("/update_certificate_page",checklogin, async function(req,res){
     res.redirect("/admin/manage_certificate_page")
 });
 router.get("/manage_project_page",async function(req,res){
-    var sql=`SELECT * FROM project_page WHERE user_id =1`;
+    var sql=`SELECT * FROM project_section WHERE id =1`;
     var data=await exe(sql);
     // console.log(data);
     var obj={"project":data[0]}
     res.render("admin/manage_project_page.ejs",obj)
 });
-router.post("/update_project_page",async function(req,res){
+router.post("/update_project_page",checkLogin,async function(req,res){
     var d=req.body;
     var sql=`UPDATE  project_page SET project_page_description = '${d.project_page_description}', project_name_1 = '${d.project_name_1}', project_link_1 = '${d.project_link_1}' , project_desc_1  = '${d.project_desc_1}', project_name_2 = '${d.project_name_2}', project_link_2 = '${d.project_link_2}'  , project_desc_2  = '${d.project_desc_2}' , project_name_3 = '${d.project_name_3}', project_link_3  ='${d.project_link_3}' , project_desc_3 = '${d.project_desc_3}' , project_name_4 = '${d.project_name_4}', project_link_4  = '${d.project_link_4}' , project_desc_4 = '${d.project_desc_4}'  , project_name_5 = '${d.project_name_5}', project_link_5 = '${d.project_link_5}' , project_desc_5 = '${d.project_desc_5}' , project_name_6 = '${d.project_name_6}', project_link_6 = '${d.project_link_6}' , project_desc_6 =  '${d.project_desc_6}'`;
      var data=await exe(sql);
     // res.send(data);
     res.redirect("/admin/manage_project_page");
 });
-router.get("/counter_section_page",async function(req,res){
+router.get("/counter_section_page",checkLogin,async function(req,res){
     var counter=await exe(`SELECT * FROM count_section WHERE user_id =1 `);
     var obj={"counter":counter[0]};
     // console.log(counter);
     res.render("admin/counter_section_page.ejs",obj);
 });
-router.post("/update_count_section",async function(req,res){
+router.post("/update_count_section",checkLogin,async function(req,res){
     var d=req.body;
     var sql=`UPDATE count_section SET  project_count = '${d.project_count}' , expe_count = '${d.expe_count}' , clients_count = '${d.clients_count}'`;
     var data=await exe(sql);
     // res.send(data);
     res.redirect("/admin/counter_section_page");
 });
-router.get("/manage_client_list",async function(req,res){
-    var client_list=await exe(`SELECT * FROM client_list WHERE user_id =1`);
-    var obj={"client_list":client_list[0]};
+router.get("/manage_client_list",checkLogin,async function(req,res){
+    var client_list=await exe(`SELECT * FROM client_information`);
+    var obj={"client_list":client_list};
     res.render("admin/manage_client_list.ejs",obj);
 });
-router.post("/update_client_list",async function(req,res){
+
+router.post("/add_client",checkLogin, async function(req,res){
     var d=req.body;
-    if(req.files)
-        {
-        var file_name=new Date().getTime()+".png";
-        req.files.client_photo_1.mv("public/uploads/"+file_name);
-        var sql2=`UPDATE client_list SET client_photo_1 = '${file_name}' WHERE user_id = 1`;
-        var data = await exe(sql2);
-        }
-        {
-            var file_name=new Date().getTime()+".png";
-            req.files.client_photo_2.mv("public/uploads/"+file_name);
-            var sql2=`UPDATE client_list SET client_photo_2 = '${file_name}' WHERE user_id = 1`;
-            var data = await exe(sql2);
-            }
-    var sql=`UPDATE  client_list SET  client_name_1 ='${d.client_name_1}', client_inform_1 = '${d.client_inform_1}' , client_name_2 = '${d.client_name_2}' , client_inform_2  = '${d.client_inform_2}' WHERE user_id = 1`;
-    var data=await exe(sql);
-    // res.send(data)
-    // console.log(data)
+    var file_name="";
+    if(req.files){
+        file_name=new Date().getTime()+"png";
+        req.files.client_photo.mv("public/uploads/"+file_name);
+        var sql=`INSERT INTO client_information(client_photo  , client_name  , client_inform ) VALUES (?,?,?)`;
+        var data=await exe(sql,[file_name,d.client_name,d.client_inform]);
+    }
+    else{
+        var sql=`INSERT INTO client_information(client_photo  , client_name  , client_inform ) VALUES (?,?,?)`;
+        var data=await exe(sql,[file_name,d.client_name,d.client_inform]);
+    }
     res.redirect("/admin/manage_client_list");
 });
-router.get("/manage_contact_information",async function(req,res){
+router.get("/delete_client/:id",checkLogin,async function(req,res){
+    var data=await exe(`DELETE FROM client_information WHERE id ='${req.params.id}'`);
+    res.redirect("/admin/manage_client_list")
+})
+
+router.get("/manage_contact_information",checkLogin,async function(req,res){
     var sql=`SELECT * FROM contact`;
     var data=await exe(sql);
     var obj={"contact":data};
     res.render("admin/manage_contact_information.ejs",obj);
 });
-router.get("/delete_contact_inquiry/:id",async function(req,res){
+router.get("/delete_contact_inquiry/:id",checkLogin,async function(req,res){
     var sql=`DELETE FROM contact WHERE user_id ='${req.params.id}'`;
     var data=await exe(sql);
     res.redirect("/admin/manage_contact_information")
 });
-router.get("/manage_myself_contact",async function(req,res){
+router.get("/manage_myself_contact",checkLogin,async function(req,res){
     var contact_myself=await exe(`SELECT * FROM contact_myself`);
     var obj={"contact_myself":contact_myself[0]};
     res.render("admin/manage_myself_contact.ejs",obj);
 });
-router.post("/update_myself_information",async function(req,res){
+router.post("/update_myself_information",checkLogin,async function(req,res){
     var d=req.body;
     var sql=`UPDATE contact_myself SET description = '${d.description}', location = '${d.location}', mobile = '${d.mobile}' , email = '${d.email}', facebook_link = '${d.facebook_link}' , instagram_id = '${d.instagram_id}', whatsapp_link = '${d.whatsapp_link}' , linked_in_link = '${d.linked_in_link}'`;
     var data=await exe(sql);
